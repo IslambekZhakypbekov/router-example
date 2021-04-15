4233d0011d2cf3f9af6993bb9609beca510049df
Islambek Zhakypbekov <islamzhakypbekov0@gmail.com>
2021-04-16 04:08:52 +0600

    fixed problems to netlify

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -10,30 +10,30 @@ const Contacts = () => {
         <div className={classes.header}>DC Comics</div>
 <div className={classes.cards}>
 <div className={classes.groot}>
-<img src={batman}/>
+<img src={batman} alt="Batman"/>
   <div className={classes.heading}>Batman</div>
   <p>Batman is a superhero who appears in American comic books published by DC Comics. Batman was created by artist Bob Kane and writer Bill Finger, and debuted in the 27th issue of the comic book Detective Comics on March 30, 1939. In the DC Universe continuity, Batman is the alias of Bruce Wayne, a wealthy American playboy, philanthropist, and owner of Wayne Enterprises based in Gotham City. Kane, Finger, and future DC writers accompanied Batman with supporting characters, including his sidekick Robin, allies Alfred Pennyworth, James Gordon and Catwoman, and foes such as the Penguin, the Riddler, the Scarecrow, and his archenemy, the Joker. Batman's origin story features him swearing vengeance against criminals after witnessing the murder of his parents Thomas and Martha; he trains himself physically and intellectually, crafts a bat-inspired persona, and monitors the Gotham streets at night.</p>
 </div>
 
 <div className={classes.deadpool}>
-<img src={superman}/>
+<img src={superman} alt="Superman"/>
 
 <div className={classes.heading}>Superman</div>
   <p>Superman is a fictional superhero, who first appeared in American comic books published by DC Comics. The character was created by writer Jerry Siegel and artist Joe Shuster, and first appeared in the comic book Action Comics #1 (cover-dated June 1938 and published April 18, 1938).[1] Superman has been adapted to a number of other media which includes radio serials, novels, movies, television shows and theatre.</p>
 </div>
 <div className={classes.Wolverine}>
-<img src={ww}/>  <div className={classes.heading}>Wonder Woman
+<img src={ww} alt="Wonder Women"/>  <div className={classes.heading}>Wonder Woman
 </div>
   <p>Wonder Woman is a fictional superheroine appearing in American comic books published by DC Comics.[3] The character is a founding member of the Justice League. The character first appeared in All Star Comics #8 in October 1941[1] with her first feature in Sensation Comics #1 in January 1942. The Wonder Woman title has been published by DC Comics almost continuously except for a brief hiatus in 1986.[4] In her homeland, the island nation of Themyscira, her official title is Princess Diana of Themyscira. When blending into the society outside of her homeland, she sometimes adopts her civilian identity Diana Prince.</p>
   </div>
 <div className={classes.spider_man}>
-<img src={gl}/>  <div className={classes.heading}>Green Lantern</div>
+<img src={gl} alt="Green Lantern"/>  <div className={classes.heading}>Green Lantern</div>
   <p>Green Lantern is the name of several superheroes appearing in American comic books published by DC Comics. They fight evil with the aid of rings that grant them a variety of extraordinary powers, all of which come from imagination and/or emotions.[citation needed] The characters are typically depicted as members of the Green Lantern Corps, an interstellar law enforcement agency.
 
 The first Green Lantern character, Alan Scott, was created in 1940 by Martin Nodell during the Golden Age of Comic Books and usually fought common criminals in Capitol City (and later, Gotham City) with the aid of his magic ring. For the Silver Age of Comic Books, John Broome and Gil Kane reinvented the character as Hal Jordan in 1959 and shifted the focus of Green Lantern stories from fantasy to science fiction. Other notable Green Lanterns include Guy Gardner, John Stewart, Kyle Rayner, Simon Baz, and Jessica Cruz.</p>
   </div>
   <div className={classes.ant_man}>
-<img src={cyborg}/>    
+<img src={cyborg} alt="Cyborg"/>    
 <div className={classes.heading}>Cyborg</div>
     <p>Cyborg (Victor Stone) is a fictional superhero appearing in American comic books published by DC Comics. The character was created by writer Marv Wolfman and artist George Pérez and first appears in a special insert in DC Comics Presents #26 (October 1980).[1] Originally known as a member of the Teen Titans,[2] Cyborg was established as a founding member of the Justice League in DC's 2011 reboot of its comic book titles.
 
--- src/components/Marvel/Marvel.js
+++ src/components/Marvel/Marvel.js
@@ -14,27 +14,27 @@ const About = () => {
  <div className={classes.header}>MARVEL</div>
 <div className={classes.cards}>
 <div className={classes.groot}>
-<img src={groot}/>
+<img src={groot} alt="Groot"/>
   <div className={classes.heading}>"I am Groot..."</div>
   <p>The last surviving member of a tree-like alien race, Groot is perhaps one of the most unusual beings in all the cosmos. Though calm and quiet in most instances, he is known to unleash his tremendous strength on any enemy foolish enough to threaten his allies, particularly his long- time partner in crime, Rocket. Though he can only vocalize using the phrase, “I am Groot,” the gentle giant’s friends are nevertheless able to understand his exact meaning through his subtle inflections.</p>
 </div>
 
 <div className={classes.deadpool}>
-<img src={deadpool}/>
+<img src={deadpool} alt="Deadpool"/>
 
 <div className={classes.heading}> Deadpool</div>
   <p>Deadpool is aware that he is a fictional comic book character. He commonly breaks the fourth wall, which is done by few other characters in the Marvel Universe, and this is used to humorous effect. ... The character, known for his talkative nature, has been nicknamed the "Merc with a Mouth". Deadpool's humor and self-awareness are what make the character unique.</p>
 </div>
 <div className={classes.Wolverine}>
-<img src={spyder}/>  <div className={classes.heading}>Wolverine</div>
+<img src={spyder} alt="Wolverine"/>  <div className={classes.heading}>Wolverine</div>
   <p>He is a mutant who possesses animal-keen senses, enhanced physical capabilities, a powerful regenerative ability known as a healing factor, and three retractable claws in each hand. Wolverine has been depicted variously as a member of the X-Men, Alpha Flight, and the Avengers.</p>
   </div>
 <div className={classes.spider_man}>
-<img src={blackspyder}/>  <div className={classes.heading}> Spider-Man</div>
+<img src={blackspyder} alt="Spyder-Man"/>  <div className={classes.heading}> Spider-Man</div>
   <p>The bite from a radioactive spider granted Peter Parker amazing arachnid-like powers. After losing his beloved Uncle Ben to a burglar, grief- stricken Peter vowed to use his incredible abilities to protect his city as the Amazing Spider-Man. Peter’s trials taught him an invaluable lesson: with great power, there must also come great responsibility.</p>
   </div>
   <div className={classes.ant_man}>
-<img src={antman}/>    
+<img src={antman} alt="Ant-Man"/>    
 <div className={classes.heading}>Ant Man</div>
     <p>Former thief Scott Lang once stole an advanced size-altering suit in order to aid his ailing daughter, only to discover that the stolen tech belonged to the world-renowned Dr. Hank Pym. Seeing the heroic potential within him, Dr. Pym allowed Scott to continue using the suit, as well as the identity Pym once battled evil under. As the Astonishing Ant-Man, Scott now handles the jobs "too small" for any other Super Hero.</p>
   </div>
